102ea99dfc2aa7356f74ec8ee82e3f393568010c
CeriOSR <CeriOSrey@gmail.com>
2017-03-11 23:29:41 -0800

    Network reachability functions

--- NetworkReachabilityTest/AppDelegate.swift
+++ NetworkReachabilityTest/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //7. add this line to start monitoring when the app opens
+        ReachabilityManager.shared.startMonitoring()
+        
         return true
     }
 
@@ -28,6 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        //8. stop the reachability monitoring
+        ReachabilityManager.shared.stopMonitoring()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
--- NetworkReachabilityTest/ViewController.swift
+++ NetworkReachabilityTest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ReachabilitySwift   //1. importing the library
 
 class ViewController: UIViewController {
 
@@ -22,4 +23,75 @@ class ViewController: UIViewController {
 
 
 }
+
+//2. create a class of type NSObject and declare a shared instance variable
+class ReachabilityManager: NSObject {
+    static let shared = ReachabilityManager()  //shared instance
+    
+    //3. declare the variable that tracks current NetworkStatus (notReachable, reachableViaWiFi, reachableViaWWAN)
+    var reachabilityStatus : Reachability.NetworkStatus = .notReachable
+    
+    //4. Bool to track network reachability
+    var isNetworkAvailable: Bool {
+        return reachabilityStatus != .notReachable
+    }
+    
+    // 5. Reachability instance for Network status monitoring
+    let reachability = Reachability()!
+    
+    ///6. Called whenever there is a change in NetworkReachibility Status
+    ///
+    /// — parameter notification: Notification with the Reachability instance
+    func reachabilityChanged(notification: Notification) {
+        let reachability = notification.object as! Reachability
+        switch reachability.currentReachabilityStatus {
+        case .notReachable:
+            debugPrint("Network became unreachable")
+        case .reachableViaWiFi:
+            debugPrint("Network reachable through WiFi")
+        case .reachableViaWWAN:
+            debugPrint("Network reachable through Cellular Data")
+        }
+    }
+    
+    ///7. Starts monitoring the network availability status
+    func startMonitoring() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.reachabilityChanged), name: ReachabilityChangedNotification, object: reachability)
+        
+        do{
+            try reachability.startNotifier()
+        } catch {
+            debugPrint("Could not start reachability notifier")
+        }
+    }
+    
+    /// Stops monitoring the network availability status
+    func stopMonitoring(){
+        reachability.stopNotifier()
+        NotificationCenter.default.removeObserver(self,
+                                                  name: ReachabilityChangedNotification,
+                                                  object: reachability)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
